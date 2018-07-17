c8b88c1bae186ecf293d4eb3a21392296511c4f4
Lucas Lang <lucaslang8@gmail.com>
2018-07-16 22:42:32 -0500

    Implemented one hot encoding for choosing the direction of the snake

--- ai.js
+++ ai.js
@@ -1,7 +1,15 @@
+var direction = ['left', 'forward', 'right'];
+
 // initial model definition
 const model = tf.sequential();
 // Initialize output layer
-model.add(tf.layers.dense({units: 1, inputShape: [3], activation: 'linear'}));
+model.add(tf.layers.dense({units: 3, inputShape: [3], activation: 'linear'}));
+
+
+let directionTensor = tf.tensor1d(direction, 'int32');;
+directionTensor.dispose();
+
+
 const adamOpt = tf.train.adam(.1);
 model.compile({
   optimizer: adamOpt,
@@ -16,19 +24,22 @@ model.compile({
 //keyPush(tf.argMax(outputs, 1).dataSync()-1);
 //outputs.print();
 
+
 async function fitModel(moveRecord) {
   console.log(moveRecord);
-  for (var i = 0; i < moveRecord.length - 1; ++i) {
+  for (var i = 0; i < moveRecord.length; i++) {
      var posArr = moveRecord[i];
-     console.log(posArr);
-     const expected = tf.tensor1d([posArr[3]]);
+     const expected = tf.oneHot(tf.tensor1d([posArr[3]], 'int32'), 3).cast('float32');
+     //console.log(expected);
      posArr.pop();
      posArr = tf.tensor2d([posArr]);
      const h = await model.fit(posArr, expected, {
          batchSize: 3,
          epochs: 1
      });
-     console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
+     //console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
+     expected.dispose();
+     posArr.dispose();
   }
 }
 
@@ -36,6 +47,6 @@ function makePrediction(input) {
   input.pop();
   let inputs = tf.tensor2d([input]);
   const outputs = model.predict(inputs);
-  console.log(outputs.print());
-  return tf.argMax(outputs, 1).dataSync()-1;
+  return direction[outputs.argMax(1).dataSync()[0]];
+
 }
